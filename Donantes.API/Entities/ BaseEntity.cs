6f654b0d9fc871f5e9b08e4034e093f0c7224643
Brenda Jaqueline Martinez Caballero <bc777209@gmail.com>
2026-07-24 10:51:29 -0600

    Agregué entidades Donador y SolicitudSangre y entidades y configuración de la base de datos

--- Donantes.API/Entities/ BaseEntity.cs
+++ Donantes.API/Entities/ BaseEntity.cs
@@ -3,6 +3,10 @@ namespace Donantes.API.Entities
 {
     public class  BaseEntity
     {
-        
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime? ModifiedDate { get; set; }
     }
 }
